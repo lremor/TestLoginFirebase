7c4bebf9a9c152c41cd579c57d9de0337443c165
lremor <luisremor@hotmail.com>
2024-07-08 22:29:59 -0300

    first commit

---
+++ Assets/Scripts/AuthManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using Firebase;
+using Firebase.Auth;
+using TMPro;
+using Firebase.Extensions;
+
+
+public class AuthManager : MonoBehaviour
+{
+
+    public static AuthManager instance;
+
+    //Firebase variables
+    [Header("Firebase")]
+    public DependencyStatus dependencyStatus;
+    public FirebaseAuth auth;
+    public FirebaseUser User;
+
+    [Header("Login")]
+    public TMP_Text statusLoginText;
+    public TMP_Text confirm1LoginText; 
+    public TMP_Text confirm2LoginText;
+    public TMP_Text confirm3LoginText;
+
+    async void Start()
+    {
+
+        Debug.Log("Checando dependencias");
+        await FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(task =>
+        {
+            dependencyStatus = task.Result;
+            statusLoginText.text = dependencyStatus.ToString();
+        });
+
+        Debug.Log("Logando anônimo");
+        confirm1LoginText.text = "Logando anônimo!";
+        var auth = FirebaseAuth.DefaultInstance;
+        await auth.SignInAnonymouslyAsync();
+
+        Debug.Log("Logado!");
+        confirm2LoginText.text = "Logado!";
+        auth.SignOut();
+        Debug.Log("Deslogado");
+        confirm3LoginText.text = "Deslogado!";
+    }
+}
